7407a7c49ff2eec9e5ce14364086222f8d727714
Lorezz <point.point@gmail.com>
2022-02-07 21:23:13 +0100

    added routes

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,2 +1,58 @@
-export * from './$';
-export { default } from './$';
+import { useLoaderData, json, useMatches, useParams, Link } from 'remix';
+import getPrefixes from '~/lib/prefixes';
+import { getPages } from '~/lib/pages';
+
+export let loader = async ({ params }) => {
+  const pages = getPages();
+  const prefixes = getPrefixes();
+  const data = { params, pages, prefixes };
+  return json(data);
+};
+
+// https://remix.run/guides/routing#index-routes
+export default function Index() {
+  let data = useLoaderData();
+  const matches = useMatches();
+  const params = useParams();
+  const { pages, prefixes } = data;
+  const locale = 'en';
+  return (
+    <div>
+      <h1>HOME</h1>
+      <ul>
+        {pages.map((item) => {
+          const prefix = prefixes[item.__typename][item.locale];
+          const url = `${prefix}/${item.slug}`;
+          return (
+            <li key={item.slug}>
+              <Link to={`${item.locale === 'en' ? '' : '/it/'}${url}`}>
+                <span>{url}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {params && (
+        <>
+          <h4>PARAMS</h4>
+          <small>
+            <code>
+              <pre>{JSON.stringify(params, null, 2)}</pre>
+            </code>
+          </small>
+        </>
+      )}
+      {matches && (
+        <>
+          <h4>MATCHES</h4>
+          <small>
+            <code>
+              <pre>{JSON.stringify(matches, null, 2)}</pre>
+            </code>
+          </small>
+        </>
+      )}
+    </div>
+  );
+}
